2fd1e7050474e5debe579951bc702f5407606906
marianouceda <marianouceda@hotmail.com>
2022-03-13 11:30:49 -0300

    armado de estructura

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const port = 5000;
+
+const publicPath = path.join(__dirname, '../', 'public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => console.log(`Servidor en línea en puerto ${port}`));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+app.get('/products', (req, res) => res.sendFile(path.resolve(__dirname, './views/products.html')));
+app.get('/cart', (req, res) => res.sendFile(path.resolve(__dirname, './views/cart.html')));
