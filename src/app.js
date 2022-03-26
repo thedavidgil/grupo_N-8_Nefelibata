d1fae656896be023bb2e668c0cf9b36892ae4322
thedavidgil <davidgil.gnz@gmail.com>
2022-03-26 13:26:43 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const confirmationRoutes =require("./routes/confirmationRoutes");
 const recoveryRoutes = require("./routes/recoveryRoutes");
 const detailRoutes = require("./routes/detailRoutes");
+const contactRoutes  =require("./routes/contactRoutes");
 app.use('/',homeRoutes);
 app.use('/register', registerRoutes);
 app.use('/login', loginRoutes);
@@ -39,18 +40,5 @@ app.use('/cart',cartRoutes);
 app.use('/confirmation',confirmationRoutes);
 app.use('/recovery',recoveryRoutes);
 app.use('/detail',detailRoutes);
+app.use('/contact',contactRoutes);
 
-<<<<<<< HEAD
-=======
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')));
-app.get('/desarrollo', (req, res) => res.sendFile(path.join(__dirname, './views/hoja_de_desarrollo.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, './views/register.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')));
-app.get('/products', (req, res) => res.sendFile(path.join(__dirname, './views/products.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, './views/contact.html')));
-app.get('/cart', (req, res) => res.sendFile(path.join(__dirname, './views/cart.html')));
-app.get('/confirmation', (req, res) => res.sendFile(path.join(__dirname, './views/confirmation.html')));
-app.get('/recovery', (req, res) => res.sendFile(path.join(__dirname, './views/password_recovery.html')));
-app.get('/detalle_producto', (req, res) => res.sendFile(path.join(__dirname, './views/detalle_producto.html')));
-app.get('/crear_editar', (req, res) => res.sendFile(path.join(__dirname, './views/crear_editar.html')));
->>>>>>> 5e245e6f45676b149106289e3422b888effddc69
