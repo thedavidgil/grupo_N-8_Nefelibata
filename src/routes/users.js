fe1df147a76387be75dd2ef9043ea1781318f504
gasbestof <galvarezb81@gmail.com>
2022-04-26 19:57:17 -0500

    Implementación de multer para el formulario de registro de usuarios

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const multer = require("multer");
 
 
@@ -10,7 +11,7 @@ const usersController = require('../controller/usersController');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, "../../public/images"))
+        cb(null, path.join(__dirname, "../../public/profileImages"))
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -21,10 +22,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
-
 /** REGISTER USER */
 router.get("/register",usersController.register);
-router.post("/register", upload.single("images"), usersController.store);
+router.post("/register", upload.single("userImage"), usersController.store);
 router.get("/confirmation", usersController.confirmation);
 router.get("/list", usersController.list);
 
