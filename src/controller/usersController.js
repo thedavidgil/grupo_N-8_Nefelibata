90144736d0bf21a2cae4a5ec655caa292fd5a58f
Sabrinaprogramadora <chabina1@gmail.com>
2022-06-14 20:21:12 -0300

    cambios en loginProcess

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -72,29 +72,34 @@ const controller = {
     return res.render("./users/login");
   },
 
-  loginProcess: (req, res) => {//otro metodo que recibirá al req, al res
+  loginProcess: async(req, res) => {//otro metodo que recibirá al req, al res
     //aca se procesa todo el formulario: tomar lo que viajo en el req del body y verificar si tengo a x persona registrada
-    let userToLogin = Users.findByField('email', req.body.email);// voy a buscar el usuario...tomo del modelo User.findByField donde digo que quiero buscar por email, y lo que vino en el body del req en el email
-    db.User.findOne({
-      where: {
-        email: req.body.email
-      }
-    })
-      .then(function (userToLogin) {
-        if (userToLogin !== null) {//si obtuve algo es true, sino es false. Ahora agrego  el then y al if  le digo que si el mail del  usuario logueado es diferente a nulo, entonces verifique la contraseña
-          let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);//encontrado el usuario hay que verificar si su contraseña , guardada en la Bd corresponde con la ingresada
+    try{
+    let userToLogin = await Users.findByField('email', req.body.email);// voy a buscar el usuario...tomo del modelo User.findByField donde digo que quiero buscar por email, y lo que vino en el body del req en el email
+    //db.User.findOne({
+      //where: {
+       // email: req.body.email
+      //}
+    //})
+      //.then(function (userToLogin) {
+        //if (userToLogin !== null) {//si obtuve algo es true, sino es false. Ahora agrego  el then y al if  le digo que si el mail del  usuario logueado es diferente a nulo, entonces verifique la contraseña
+          let isOkThePassword = await bcryptjs.compareSync(req.body.password, userToLogin.password);//encontrado el usuario hay que verificar si su contraseña , guardada en la Bd corresponde con la ingresada
           if (isOkThePassword) {//si todo esta bien, antes de redirigir a la persona quiero guardar al usuario en session, para eso se hace la linea 56
             delete userToLogin.password;//antes de pasar el usuario en sesion es borrar del userToLogin la propiedad password. Es solo por seguridad. Con delete podemos borrar una propiedad determinada. Se borra la contraseña. No se quiere mantener en session toda la info del usuario. Si interesa el fullname, email
             req.session.userLogged = userToLogin;//session es la parte que nos va a permitir implementar a lo largo de toda nuestra aplicacion la variable session que es objeto literal que va a contener info que yo quiera (se instala express-session. npm install express-session) luego inicializo la sesion en app.js
             if (req.body.remember_user) {//si en el req, en el body vino remember_user
               res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 2 })//proceso para recordar el usuario. seteo una cookie. En el res voy a setaer una cookie userEmail y lo que va a guardar la cookie es el valor de lo que vino en el body del req que es la propiedad email. Esto va a durar maxAge es para indicar que la cookie dura 1000 milisegundos pero lo multiplica por 60 que son 60 segundos. va a userLoggedMiddleware
+            //}
+         // }
+        //}
             }
           }
-        }
         return res.redirect("profile");//si da verdadero, redirigir a /user/profile. o sea, si los datos ingresados son correctos me llevan a userProfile
-      })
-      .then((response) => {
-        if (response)
+      //})
+        }
+            catch (err){
+              console.error(err)
+            }
           return res.render("./users/login", {//cuando no se obtiene nada (undefined)
             errors: {//crear un objeto literal
               email: {//va a tener un error para el email
@@ -102,7 +107,6 @@ const controller = {
               }
             }
           })
-      })
       .then((response) => {
         if (response)
           return res.render("./users/login", {
@@ -112,8 +116,9 @@ const controller = {
               }
             }
           })
-      })
-  },
+        })
+      },
+
 
   //??????????????????????????????
   edit: function (req, res) {//Sabrina. Es muy parecido al de crear. La diferencia es que como quiero editar un usuario tengo que mandar los datos del usuario para que llegue y se autocomplete el formulario
@@ -123,7 +128,8 @@ const controller = {
       .then(function (Users) {//Sabrina
         res.render("./users/edit", { Users });
       })
-  },
+    },
+
 
   update: (req, res) => {//???????????????????
     const id = req.params.id;//Sabrina. seria ideal poner el req params aca para validar que no me esta llegando cualquier cosa en lugrar de ponerlo en el where. Sería así: const id = req.params.id; sería la mejor forma de validar el id que nos llega
