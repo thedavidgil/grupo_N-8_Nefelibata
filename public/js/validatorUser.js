f8939777c9089c6abe3c4e06c5df8c056d1f4176
marianouceda <marianouceda@hotmail.com>
2022-06-30 00:32:30 -0300

    spring 7 - validatorUser

---
+++ public/js/validatorUser.js
@@ -0,0 +1,79 @@
+// User register
+const db = require('../../database/models')
+const path = require('path');
+
+window.addEventListener('load', function(){
+    let btnSubmit = document.querySelector('.button1');
+    let registerForm = document.querySelector('.form1');
+
+    btnSubmit.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let errores = {};
+
+        let firstName = document.getElementById('firstName');
+        let erFirstName = document.querySelector('.erFirstName');
+        if(firstName.value = ''){
+            errores.firstName = 'Tenes que escribir un nombre'
+        } else if (firstName.value.lenght < 2){
+            errores.firstName = 'El nombre debe contener al menos 2 caracteres'
+        };
+
+        let lastName = document.getElementById('lastName');    
+        let erLastName = document.querySelector('.erLastName');
+        if (lastName.value = '') {
+            errores.lastName = 'Tenes que escribir un nombre';
+        } else if (lastName.value.lenght < 2){
+            errores.lastName = 'El nombre debe contener al menos 2 caracteres';
+        };
+
+        let email = document.getElementById('email');
+        let erEmail = document.querySelector('.erEmail');
+        if (email.value = '') {
+            errores.email = 'Tenes que escribir un correo electrónico';
+        } else if (email.value.indexOf('@') == -1) {
+            errores.email = 'Tenes que escribir un correo electrónico válido';
+        };
+
+        db.User.findOne({
+            where: {
+              email: req.body.email
+            }
+        }).then(result => {
+            if (result !== null) {
+                errores.email = 'Este email ya está registrado';
+            };
+        });
+
+        let password = document.getElementById('password');
+        let erPassword = document.querySelector('.erPassword');
+        if (password.value = '') {
+            errores.password = 'Tenes que escribir una contraseña';
+        }; //else if (password.value.match(/[0-9]/[A-Z]/[a-z]/[])){
+
+        let repassword = document.getElementById('repassword');
+        let erRepassword = document.querySelector('.erRepassword');
+        if (repassword.value = '') {
+            errores.repassword = 'Tenes que reescribir la contraseña';
+        };
+            
+        let avatar = document.getElementById('avatar');
+        let erAvatar = document.querySelector('.erAvatar');
+        let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+        if (!avatar.value.includes(acceptedExtensions)) {
+            errores.password = `Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`;
+        };
+
+        if (Object.keys(errores).length >= 1) {
+            erFirstName.innerText = (errores.firstName) ? errores.firstName : '';
+            erLastName.innerText = (errores.lastName) ? errores.lastName : '';
+            erEmail.innerText = (errores.email) ? errores.email : '';
+            erPassword.innerText = (errores.password) ? errores.password : '';
+            erRepassword.innerText = (errores.repassword) ? errores.repassword : '';
+            erAvatar.innerText = (errores.avatar) ? errores.avatar : '';
+        } else {
+            registerForm.submit();
+        }
+
+    });
+});
--- src/middlewares/validationRegisterMiddleware.js
+++ src/middlewares/validationRegisterMiddleware.js
@@ -4,26 +4,26 @@ const { body } = require('express-validator');
 
 module.exports = [
 	body('firstName')
-		.notEmpty().withMessage('Tienes que escribir tu Nombre').bail()
-		.isLength({ min: 2 }).withMessage("El Nombre deberá contener al menos 2 caracteres"),	
+		.notEmpty().withMessage('Tenes que escribir un nombre').bail()
+		.isLength({ min: 2 }).withMessage("El nombre debe contener al menos 2 caracteres"),	
   	body('lastName')
-  		.notEmpty().withMessage('Tienes que escribir tu Apellido').bail()
-		.isLength({ min: 2 }).withMessage("El Apellido deberá contener al menos 2 caracteres"),
+  		.notEmpty().withMessage('Tenes que escribir un apellido').bail()
+		.isLength({ min: 2 }).withMessage("El apellido debe contener al menos 2 caracteres"),
 	body('email')
-		.notEmpty().withMessage('Tienes que escribir tu correo electrónico').bail()
-		.isEmail().withMessage('Debes escribir un correo electronico válido'),
+		.notEmpty().withMessage('Tenes que escribir un correo electrónico').bail()
+		.isEmail().withMessage('Tenes que escribir un correo electrónico válido'),
 	body('password')
-		.notEmpty().withMessage('Tienes que escribir una contraseña').bail()
+		.notEmpty().withMessage('Tenes que escribir una contraseña').bail()
 		.isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1}).withMessage('La contraseña debe contener 8 dígitos con al menos una letra mayúscula, una minúsculas, un número y un carácter especial'),
 	body('repassword')
-		.notEmpty().withMessage('Tienes que reescribir la contraseña').bail()
+		.notEmpty().withMessage('Tenes que reescribir la contraseña').bail()
 		.equals('password'),
 	body('userImage').custom((value, { req }) => {
 		let file = req.file;
 		let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
 
 		if (!file) {
-			throw new Error('Tienes que subir una imagen de avatar');
+			throw new Error('Tenes que subir una imagen de avatar');
 		} else {
 			let fileExtension = path.extname(file.originalname);
 			if (!acceptedExtensions.includes(fileExtension)) {
