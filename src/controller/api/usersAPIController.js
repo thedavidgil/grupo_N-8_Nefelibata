e8ff1274673c749f6e7b9c1770b930328078a63e
Sabrinaprogramadora <chabina1@gmail.com>
2022-07-05 23:27:04 -0300

    userApiController

---
+++ src/controller/api/usersAPIController.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const db = require("/database/models");
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+
+
+const usersApiController = {
+    "listado": (req, res) => {
+        db.User.findAll()//el modelo a consultar, todos los usuarios
+        .then(user => {//recibe a los usuarios
+            let respuesta = {//la respuesta en metodo json. Este formato json permite enviar la info de este modo para poder ser consumida como si fuese una Api
+                meta: {
+                    count: user.length,//se obtiene el total de usuarios
+                    id: user.id,
+                    name: user.first_name,
+                    email: user.email,
+                    detail: "api/user",
+                    status : 200,//si el valor es satisfactorio
+                },
+                data: user//se obtiene la data
+            }
+                res.json(respuesta);
+            })
+    },
+    "detalle": (req, res) => {
+        db.Users.findByPk(req.params.id)
+            .then(user => {
+                let respuesta = {
+                    meta: {
+                    id: user.id,
+                    name: user.first_name,
+                    email: user.email,
+                    url: "/api/avatar_id",
+                    status: 200,
+                    },
+                    data: user
+                }
+                res.json(respuesta);
+            });
+    }
+}
+
+    module.exports = usersApiController;
