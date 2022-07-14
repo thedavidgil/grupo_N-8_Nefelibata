2516980b89c96f17bca93aa35d07d1e4947b8e7a
marianouceda <marianouceda@hotmail.com>
2022-07-14 20:00:25 -0300

    S8 - vistas, funcionalidad

--- public/js/validatorUser.js
+++ public/js/validatorUser.js
@@ -1,3 +1,17 @@
+let avatar = document.getElementById('avatar');
+let chosenImage = document.getElementById('chosen-image');
+let imageName = document.getElementById('image-name');
+
+avatar.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(avatar.files[0]);
+    console.log(avatar.files[0]);
+    reader.onload = () => {
+        chosenImage.setAttribute('src',reader.result);
+    }
+    imageName.textContent = avatar.files[0].name;
+};
+
 // User register
 const db = require('../../database/models')
 
@@ -75,4 +89,5 @@ window.addEventListener('load', function(){
         }
 
     });
+
 });
