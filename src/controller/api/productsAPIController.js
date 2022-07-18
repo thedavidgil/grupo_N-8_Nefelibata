5cf8e8adcb142f8c7cb37ad105451762e2b31527
marianouceda <marianouceda@hotmail.com>
2022-07-18 18:56:42 -0300

    S8 correccion en respuesta de property lastest

--- src/controller/api/productsAPIController.js
+++ src/controller/api/productsAPIController.js
@@ -5,9 +5,14 @@ const controller = {
         let count = await db.sequelize.query('SELECT COUNT(product_id) FROM Products;');
         let countByCategory = await db.sequelize.query('SELECT Product_category.category as "product_category", COUNT(*) as "count" FROM `Products` INNER JOIN Product_category ON Products.product_category_id = Product_category.product_category_id GROUP BY category;');
         let products = await db.Product.findAll({
-                        include: ['product_categories'],
-                        attributes: ["product_id", "product_name", "description", "product_categories.category"],});
+            include: ['product_categories'],
+            attributes: ["product_id", "product_name", "description", "product_categories.category"],});
+        
         let productsTable = await db.sequelize.query('SELECT product_id, product_name, description, product_category.category FROM `products` INNER JOIN Product_category ON products.product_category_id = Product_category.product_category_id;')
+        // let productWithImage = await db.sequelize.query('SELECT products.product_id, products.product_name, products.description, product_category.category, product_images.image FROM `products` INNER JOIN Product_category ON products.product_category_id = Product_category.product_category_id INNER JOIN product_images ON product_images.product_id = Products.product_id GROUP BY products.product_id ORDER BY products.product_id DESC;')
+        let productWithImage = await db.Product.findAll({
+            include: ['product_categories', 'product_images'],
+            attributes: ["product_id", "product_name", "description", "product_categories.category", "product_images.image"],});
         let countCategories = await db.sequelize.query('SELECT COUNT(product_category_id) FROM Product_category;');
         
         count = count[0];
@@ -15,10 +20,10 @@ const controller = {
         countCategories = countCategories[0];
         products.map( product => product.dataValues.detail = `http://localhost:5000/api/products/${product.dataValues.product_id}`)
         products = [...products]
+        // productWithImage.map( product => product.dataValues)
+        // productWithImage = [...productWithImage]
 
-        let lastest = products[products.length-1];
-
-        let arrProducts = Object.entries(products);
+        let lastest = productWithImage[productWithImage.length-1];
 
         return res.status(200).json({ 
             meta: {
@@ -28,6 +33,7 @@ const controller = {
                 count, 
                 countByCategory, 
                 products,
+
                 productsTable,
                 lastest,
                 countCategories
