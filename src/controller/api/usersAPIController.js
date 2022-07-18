40d84fb0f74a5995ff32a8423a7856e3e99cb27a
Sabrinaprogramadora <chabina1@gmail.com>
2022-07-18 01:07:34 -0300

    retro

--- src/controller/api/usersAPIController.js
+++ src/controller/api/usersAPIController.js
@@ -1,40 +1,4 @@
 const db = require('../../../database/models');
-<<<<<<< HEAD
-
-const usersApiController = {
-    list: async (req, res) =>{
-        let users = await db.User.findAll({ attributes: ["user_id", "first_name", "last_name", "email"] });
-        users.map(user => user.dataValues.detail = `http://localhost:5000/api/users/${ user.dataValues.user_id }`)
-        users = [...users ];
-
-        let lastest = users[users.length-1];
-
-        return res.status(200).json({
-            meta: {
-                status: 200
-            },
-            data: {
-                users,
-                lastest
-            }
-        });
-    },
-
-    detail: async (req, res) => {
-        let users = await db.User.findAll({ include:["avatars"], attributes: ["user_id", "first_name", "last_name", "email"] });
-        users = { ...users }
-        return res.status(200).json({
-            meta: {
-                status: 200
-            },
-            data: {
-                users
-            }
-        });
-    }
-}
-module.exports = usersApiController;
-=======
 const sequelize = db.sequelize;
 
 const usersApiController = {
@@ -203,4 +167,3 @@ db.Users.findByPk(req.params.id)
     }
 }
 module.exports = usersAPIController;*/
->>>>>>> 1920842ed39f3a479de9a397bf3e026a1494503f
